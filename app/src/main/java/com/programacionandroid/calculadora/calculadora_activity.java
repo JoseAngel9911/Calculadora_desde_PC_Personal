3f04691c67903aa2a29dfb06ffb00bf99d02de11
Joke-Angel <83519426+Joke-Angel@users.noreply.github.com>
2022-09-12 20:23:46 -0300

    Commit inicial, subiendo proyecto.

---
+++ app/src/main/java/com/programacionandroid/calculadora/calculadora_activity.java
@@ -0,0 +1,16 @@
+package com.programacionandroid.calculadora;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class calculadora_activity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal);
+    }
+
+}
